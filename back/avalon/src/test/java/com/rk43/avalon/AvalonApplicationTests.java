12592e7987677df370ba091aabd73a1f7743f7b9
haZuny <gkwns5791@naver.com>
2024-08-18 16:46:07 +0900

    init project

---
+++ back/avalon/src/test/java/com/rk43/avalon/AvalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk43.avalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
